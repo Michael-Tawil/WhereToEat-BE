18c4d61fd5d83a0af2fbe6f9996c7dd33566ed9e
Max <Max@DESKTOP-AF9U044>
2026-04-05 15:26:16 +1000

    Add project files.

---
+++ Controllers/AISuggestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhereToEat_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AISuggestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetSuggestion() => Ok(new[] { "test from API" });
+    }
+}
